0aed24ccaed8bdd60dbe88c3896a322a088c34bb
Sergey Nazarenko <djfezsergey@mail.ru>
2018-11-23 18:46:13 +0200

    9

--- src/test/java/com/itstep/caloriesmanager/service/AbstractUserServiceTest.java
+++ src/test/java/com/itstep/caloriesmanager/service/AbstractUserServiceTest.java
@@ -102,4 +102,13 @@ import static com.itstep.caloriesmanager.UserTestData.*;
         List<User> all = service.getAll();
         assertMatch(all, ADMIN, USER);
     }
+
+    @Test
+    public void testValidation() throws Exception {
+        validateRootCause(() -> service.create(new User(null, "  ", "mail@yandex.ru", "password", Role.ROLE_USER)), ConstraintViolationException.class);
+        validateRootCause(() -> service.create(new User(null, "User", "  ", "password", Role.ROLE_USER)), ConstraintViolationException.class);
+        validateRootCause(() -> service.create(new User(null, "User", "mail@yandex.ru", "  ", Role.ROLE_USER)), ConstraintViolationException.class);
+        validateRootCause(() -> service.create(new User(null, "User", "mail@yandex.ru", "password", 9, true, new Date(), Collections.emptySet())), ConstraintViolationException.class);
+        validateRootCause(() -> service.create(new User(null, "User", "mail@yandex.ru", "password", 10001, true, new Date(), Collections.emptySet())), ConstraintViolationException.class);
+    }
 }
